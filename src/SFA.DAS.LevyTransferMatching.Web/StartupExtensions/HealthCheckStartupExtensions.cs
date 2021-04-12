859647cbccbd3c20a36ea6f29e5068b4c6cf8b34
chrisfoster76 <chrisfoster186@googlemail.com>
2021-04-12 11:01:10 +0100

    Renames a file

--- src/SFA.DAS.LevyTransferMatching.Web/StartupExtensions/HealthCheckStartupExtensions.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/StartupExtensions/HealthCheckStartupExtensions.cs
@@ -1,13 +1,14 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.LevyTransferMatching.Web.Extensions;
 using SFA.DAS.LevyTransferMatching.Web.HealthChecks;
 
 namespace SFA.DAS.LevyTransferMatching.Web.StartupExtensions
 {
-    public static class HealthChecksStartup
+    public static class HealthCheckStartupExtensions
     {
         public static IServiceCollection AddDasHealthChecks(this IServiceCollection services)
         {
@@ -20,6 +21,16 @@ namespace SFA.DAS.LevyTransferMatching.Web.StartupExtensions
 
         public static IApplicationBuilder UseDasHealthChecks(this IApplicationBuilder app)
         {
+            app.UseHealthChecks("/ping", new HealthCheckOptions
+            {
+                Predicate = (_) => false,
+                ResponseWriter = (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+                    return context.Response.WriteAsync("");
+                }
+            });
+
             return app.UseHealthChecks("/health", new HealthCheckOptions
             {
                 ResponseWriter = (c, r) => c.Response.WriteJsonAsync(new
