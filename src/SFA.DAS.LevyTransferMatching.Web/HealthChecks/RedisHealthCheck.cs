b730a3e22d51e96c7278061811a672e0128d2d17
dannygb <dan.beavon@education.gov.uk>
2021-05-04 13:22:25 +0100

    [TM-9] Adds Redis health check

---
+++ src/SFA.DAS.LevyTransferMatching.Web/HealthChecks/RedisHealthCheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SFA.DAS.LevyTransferMatching.Infrastructure.Configuration;
+using StackExchange.Redis;
+
+namespace SFA.DAS.LevyTransferMatching.Web.HealthChecks
+{
+    //public class RedisHealthCheck
+    //{
+    //    private readonly string _redisConnectionString;
+
+    //    public RedisHealthCheck(LevyTransferMatchingWeb config)
+    //    {
+    //        _redisConnectionString = config.RedisConnectionString;
+    //    }
+
+    //    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+    //        CancellationToken cancellationToken = new CancellationToken())
+    //    {
+    //        try
+    //        {
+    //            var conn = await ConnectionMultiplexer.ConnectAsync(_redisConnectionString);
+    //            var sub = conn.GetSubscriber();
+    //            var timeSpan = await sub.PingAsync();
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return HealthCheckResult.Unhealthy("Redis ping", ex);
+    //        }
+
+    //        return HealthCheckResult.Healthy("Redis ping", new Dictionary<string, object>());
+    //    }
+    //}
+}
