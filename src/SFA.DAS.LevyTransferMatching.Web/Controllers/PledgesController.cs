e92074bf4c746e77db9e5ee0847d5035819b0498
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-05-07 11:48:09 +0100

    Initial pages, pre-auth stuff.

---
+++ src/SFA.DAS.LevyTransferMatching.Web/Controllers/PledgesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.LevyTransferMatching.Web.Controllers
+{
+    public class PledgesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public PledgesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
