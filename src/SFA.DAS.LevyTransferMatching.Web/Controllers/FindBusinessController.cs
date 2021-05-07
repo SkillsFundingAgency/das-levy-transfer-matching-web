236edcd9a6647a70777295afea6a918629577864
dannygb <dan.beavon@education.gov.uk>
2021-05-07 11:44:16 +0100

    [TM-28] - Adds page with content

---
+++ src/SFA.DAS.LevyTransferMatching.Web/Controllers/FindBusinessController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SFA.DAS.LevyTransferMatching.Web.Controllers
+{
+    [AllowAnonymous]
+    public class FindBusinessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
