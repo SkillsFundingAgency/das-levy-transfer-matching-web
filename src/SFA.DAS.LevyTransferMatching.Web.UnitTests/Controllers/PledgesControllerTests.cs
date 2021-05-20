34943127b4a7a8a2c0df6b7321e9725914f57a29
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-05-20 11:05:06 +0100

    Unit test tweaking based on feedback.

--- src/SFA.DAS.LevyTransferMatching.Web.UnitTests/Controllers/PledgesControllerTests.cs
+++ src/SFA.DAS.LevyTransferMatching.Web.UnitTests/Controllers/PledgesControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using SFA.DAS.LevyTransferMatching.Web.Controllers;
 using SFA.DAS.LevyTransferMatching.Web.Models.Pledges;
@@ -8,25 +9,25 @@ namespace SFA.DAS.LevyTransferMatching.Web.UnitTests.Controllers
     [TestFixture]
     public class PledgesControllerTests
     {
-        private PledgesController pledgesController;
+        private Fixture _fixture;
+        private PledgesController _pledgesController;
 
         [SetUp]
         public void Setup()
         {
-            pledgesController = new PledgesController();
+            _fixture = new Fixture();
+            _pledgesController = new PledgesController();
         }
 
         [Test]
         public void GET_Index_Returns_Expected_View_With_Expected_ViewModel()
         {
             // Arrange
-            string encodedAccountId = "abc";
-            ViewResult viewResult = null;
-            IndexViewModel indexViewModel = null;
+            string encodedAccountId = _fixture.Create<string>();
 
             // Act
-            viewResult = this.pledgesController.Index(encodedAccountId) as ViewResult;
-            indexViewModel = viewResult?.Model as IndexViewModel;
+            ViewResult viewResult = _pledgesController.Index(encodedAccountId) as ViewResult;
+            IndexViewModel indexViewModel = viewResult?.Model as IndexViewModel;
 
             // Assert
             Assert.NotNull(viewResult);
@@ -38,13 +39,11 @@ namespace SFA.DAS.LevyTransferMatching.Web.UnitTests.Controllers
         public void GET_Create_Returns_Expected_View_With_Expected_ViewModel()
         {
             // Arrange
-            string encodedAccountId = "abc";
-            ViewResult viewResult = null;
-            CreateViewModel createViewModel = null;
+            string encodedAccountId = _fixture.Create<string>();
 
             // Act
-            viewResult = this.pledgesController.Create(encodedAccountId) as ViewResult;
-            createViewModel = viewResult?.Model as CreateViewModel;
+            ViewResult viewResult = _pledgesController.Create(encodedAccountId) as ViewResult;
+            CreateViewModel createViewModel = viewResult?.Model as CreateViewModel;
 
             // Assert
             Assert.NotNull(viewResult);
