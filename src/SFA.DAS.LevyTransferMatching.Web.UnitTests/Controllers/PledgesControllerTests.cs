77dfc58cf33b08ab4e693853a38f5eaa02940cdc
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-05-20 09:23:58 +0100

    Tweaks & unit tests

---
+++ src/SFA.DAS.LevyTransferMatching.Web.UnitTests/Controllers/PledgesControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Web.Controllers;
+using SFA.DAS.LevyTransferMatching.Web.Models.Pledges;
+
+namespace SFA.DAS.LevyTransferMatching.Web.UnitTests.Controllers
+{
+    [TestFixture]
+    public class PledgesControllerTests
+    {
+        private PledgesController pledgesController;
+
+        [SetUp]
+        public void Setup()
+        {
+            pledgesController = new PledgesController();
+        }
+
+        [Test]
+        public void GET_Index_Returns_Expected_View_With_Expected_ViewModel()
+        {
+            // Arrange
+            string encodedAccountId = "abc";
+            ViewResult viewResult = null;
+            IndexViewModel indexViewModel = null;
+
+            // Act
+            viewResult = this.pledgesController.Index(encodedAccountId) as ViewResult;
+            indexViewModel = viewResult?.Model as IndexViewModel;
+
+            // Assert
+            Assert.NotNull(viewResult);
+            Assert.NotNull(indexViewModel);
+            Assert.AreEqual(encodedAccountId, indexViewModel.EncodedAccountId);
+        }
+
+        [Test]
+        public void GET_Create_Returns_Expected_View_With_Expected_ViewModel()
+        {
+            // Arrange
+            string encodedAccountId = "abc";
+            ViewResult viewResult = null;
+            CreateViewModel createViewModel = null;
+
+            // Act
+            viewResult = this.pledgesController.Create(encodedAccountId) as ViewResult;
+            createViewModel = viewResult?.Model as CreateViewModel;
+
+            // Assert
+            Assert.NotNull(viewResult);
+            Assert.NotNull(createViewModel);
+            Assert.AreEqual(encodedAccountId, createViewModel.EncodedAccountId);
+        }
+    }
+}
--- src/SFA.DAS.LevyTransferMatching.Web/Controllers/PledgesController.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Controllers/PledgesController.cs
@@ -2,44 +2,33 @@
 using Microsoft.AspNetCore.Routing;
 using SFA.DAS.Authorization.EmployerUserRoles.Options;
 using SFA.DAS.Authorization.Mvc.Attributes;
-using SFA.DAS.LevyTransferMatching.Web.Authorization;
 using SFA.DAS.LevyTransferMatching.Web.Models.Pledges;
 
 namespace SFA.DAS.LevyTransferMatching.Web.Controllers
 {
-    // [DasAuthorize(EmployerUserRole.OwnerOrTransactor)]
+    [DasAuthorize(EmployerUserRole.OwnerOrTransactor)]
     [Route("accounts/{EncodedAccountId}/pledges")]
     public class PledgesController : Controller
     {
-        public PledgesController()
+        public IActionResult Index(string encodedAccountId)
         {
-        }
-
-        public IActionResult Index()
-        {
-            var viewModel = this.CreateViewModel<IndexViewModel>(this.RouteData);
+            var viewModel = new IndexViewModel()
+            {
+                EncodedAccountId = encodedAccountId,
+            };
 
             return View(viewModel);
         }
 
         [Route("create")]
-        public IActionResult Create()
+        public IActionResult Create(string encodedAccountId)
         {
-            var viewModel = this.CreateViewModel<CreateViewModel>(this.RouteData);
-
-            return View(viewModel);
-        }
-
-        private TPledgesViewModel CreateViewModel<TPledgesViewModel>(RouteData routeData) where TPledgesViewModel : PledgesViewModel, new()
-        {
-            string encodedAccountId = (string)routeData.Values[RouteValueKeys.EncodedAccountId];
-
-            TPledgesViewModel pledgesViewModel = new TPledgesViewModel()
+            var viewModel = new CreateViewModel()
             {
                 EncodedAccountId = encodedAccountId,
             };
 
-            return pledgesViewModel;
+            return View(viewModel);
         }
     }
 }
