70ac46d443215fe37e7c99a13d24a493962e9ad2
Conor Till <conortill@gmail.com>
2021-06-02 11:22:06 +0100

    Adding job role criteria

--- src/SFA.DAS.LevyTransferMatching.Web/Models/Pledges/CreateViewModel.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Models/Pledges/CreateViewModel.cs
@@ -11,8 +11,10 @@ namespace SFA.DAS.LevyTransferMatching.Web.Models.Pledges
         public int? Amount { get; set; }
         public bool? IsNamePublic { get; set; }
         public Sector? Sectors { get; set; }
+        public JobRole? JobRoles { get; set; }
 
         public string IsNamePublicDisplayValue => IsNamePublic.HasValue ? IsNamePublic.Value ? "Show" : "Hide" : "-";
         public bool AreAllSectorsSelected => !Sectors.HasValue || Sectors == Sector.None || Sectors == EnumExtensions.GetMaxValue<Sector>();
+        public bool AreAllJobRolesSelected => !JobRoles.HasValue || JobRoles == JobRole.None || JobRoles == EnumExtensions.GetMaxValue<JobRole>();
     }
 }
--- src/SFA.DAS.LevyTransferMatching.Web/Orchestrators/IPledgeOrchestrator.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Orchestrators/IPledgeOrchestrator.cs
@@ -9,7 +9,9 @@ namespace SFA.DAS.LevyTransferMatching.Web.Orchestrators
         Task<CreateViewModel> GetCreateViewModel(CreateRequest request);
         Task<AmountViewModel> GetAmountViewModel(AmountRequest request);
         Task<SectorViewModel> GetSectorViewModel(SectorRequest request);
+        Task<JobRoleViewModel> GetJobRoleViewModel(JobRoleRequest request);
         Task UpdateCacheItem(AmountPostRequest request);
         Task UpdateCacheItem(SectorPostRequest request);
+        Task UpdateCacheItem(JobRolePostRequest request);
     }
 }
