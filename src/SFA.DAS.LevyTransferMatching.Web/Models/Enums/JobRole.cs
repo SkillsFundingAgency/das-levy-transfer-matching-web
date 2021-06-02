29ae0ece8150aecc8240d386f9f31580fc9d3aaa
Conor Till <conortill@gmail.com>
2021-06-02 14:57:37 +0100

    Content changes for Job Role page

--- src/SFA.DAS.LevyTransferMatching.Web/Models/Enums/JobRole.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Models/Enums/JobRole.cs
@@ -9,11 +9,11 @@ namespace SFA.DAS.LevyTransferMatching.Web.Models.Enums
         None = 0,
         [Display(Name = "Agriculture, environmental and animal care", Description = "For example, Veterinary Nurse, Horticulture and Landscape Operative, Equine Groom, Golf Greenkeeper")]
         Agriculture = 1,
-        [Display(Name = "Business and administration", Description = "For example, Team Leader or Supervisor, Operations or Departmental Manager, Business Administrator, Senior Leader (degree)")]
+        [Display(Name = "Business and administration", Description = "For example, Team Leader or Supervisor, Operations or Departmental Manager, Business Administrator, HR support")]
         Business = 2,
-        [Display(Name = "Care services", Description = "For example, Lead Adult Care Worker, Adult Care Worker, Children, Young People and Families Practitioner, Children, Young People and Families Manager")]
+        [Display(Name = "Care services", Description = "For example, Social worker (degree), Adult Care Worker, Children, Young People and Families Practitioner, Youth support worker")]
         CareServices = 4,
-        [Display(Name = "Catering and hospitality", Description = "For example, Hospitality Team Member, Hospitality Supervisor, Commis Chef, Production Chef")]
+        [Display(Name = "Catering and hospitality", Description = "For example, Hospitality Team Member, Hospitality Supervisor, Commis Chef, Production Chef, Baker")]
         Catering = 8,
         [Display(Name = "Construction", Description = "For example, Installation Electrician or Maintenance Electrician, Carpentry and Joinery, Chartered Surveyor (degree), Bricklayer")]
         Construction = 16,
@@ -23,19 +23,19 @@ namespace SFA.DAS.LevyTransferMatching.Web.Models.Enums
         Digital = 64,
         [Display(Name = "Education and childcare", Description = "For example, Early Years Educator, Teaching Assistant, Academic Professional, Learning and Skills Teacher")]
         Education = 128,
-        [Display(Name = "Engineering and manufacturing", Description = "For example, Engineering Technician, Motor Vehicle Service and Maintenance Technician, Maintenance and Operations Engineering Technician, Heavy Vehicle Service and Maintenance Technician")]
+        [Display(Name = "Engineering and manufacturing", Description = "For example, Engineering Technician, Motor Vehicle Service and Maintenance Technician, Metal fabricator, Aerospace engineer (degree)")]
         Engineering = 256,
         [Display(Name = "Hair and beauty", Description = "For example, Hair Professional, Beauty Therapist, Nail Services Technician")]
         Hair = 512,
-        [Display(Name = "Health and science", Description = "For example, Senior Healthcare Support Worker, Dental Nurse, Nursing Associate, Healthcare Support Worker")]
+        [Display(Name = "Health and science", Description = "For example, Healthcare Support Worker, Dental Nurse, Nursing Associate, Midwife (degree), Laboratory technician")]
         Health = 1024,
-        [Display(Name = "Legal, finance and accounting", Description = "For example, Accountancy or Taxation Professional, Assistant Accountant, Professional Accounting or Taxation Technician, Insurance Practitioner")]
+        [Display(Name = "Legal, finance and accounting", Description = "For example, Assistant Accountant, Mortgage adviser, Payroll administrator, Solicitor, Insurance Practitioner")]
         Legal = 2048,
         [Display(Name = "Protective services", Description = "For example, HM Forces Serviceperson, Police Constable, Operational Firefighter, Custody and Detention Officer")]
         ProtectiveServices = 4096,
-        [Display(Name = "Sales, marketing and procurement", Description = "For example, Customer Service Practitioner, Retailer, Digital Marketer, Retail Team Leader")]
+        [Display(Name = "Sales, marketing and procurement", Description = "For example, Customer Service Practitioner, Digital Marketer, Retail Team Leader, Junior estate agent")]
         Sales = 8192,
-        [Display(Name = "Transport and logistics", Description = "For example, Large Goods Vehicle (LGV) Driver, Supply Chain Warehouse Operative, Passenger Transport Driver, Cabin Crew")]
+        [Display(Name = "Transport and logistics", Description = "For example, Large Goods Vehicle (LGV) Driver, Supply Chain Warehouse Operative, Cabin Crew, Train driver")]
         Transport = 16384
     }
 }
