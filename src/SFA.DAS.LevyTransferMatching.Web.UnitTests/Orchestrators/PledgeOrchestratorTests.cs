877d8d56361a46162740a3fe1d3b5a4d06dcc090
Conor Till <conortill@gmail.com>
2021-06-02 14:30:09 +0100

    Adding tests

--- src/SFA.DAS.LevyTransferMatching.Web.UnitTests/Orchestrators/PledgeOrchestratorTests.cs
+++ src/SFA.DAS.LevyTransferMatching.Web.UnitTests/Orchestrators/PledgeOrchestratorTests.cs
@@ -93,6 +93,19 @@ namespace SFA.DAS.LevyTransferMatching.Web.UnitTests.Orchestrators
         }
 
         [Test]
+        public async Task GetCreateViewModel_JobRoles_Is_Retrieved_From_Cache()
+        {
+            var cacheItem = _fixture.Build<CreatePledgeCacheItem>()
+                .With(x => x.JobRoles, TestHelper.GetRandomFlagsValue<JobRole>())
+                .Create();
+
+            _cache.Setup(x => x.RetrieveFromCache<CreatePledgeCacheItem>(_cacheKey.ToString())).ReturnsAsync(cacheItem);
+
+            var result = await _orchestrator.GetCreateViewModel(new CreateRequest { EncodedAccountId = _encodedAccountId, CacheKey = _cacheKey });
+            Assert.AreEqual(cacheItem.JobRoles, result.JobRoles);
+        }
+
+        [Test]
         public async Task GetAmountViewModel_EncodedId_Is_Correct()
         {
             var result = await _orchestrator.GetAmountViewModel(new AmountRequest { EncodedAccountId = _encodedAccountId, CacheKey = _cacheKey });
@@ -151,6 +164,33 @@ namespace SFA.DAS.LevyTransferMatching.Web.UnitTests.Orchestrators
 
             var result = await _orchestrator.GetSectorViewModel(new SectorRequest { EncodedAccountId = _encodedAccountId, CacheKey = _cacheKey });
             Assert.AreEqual(cacheItem.Sectors, result.Sectors);
+        }
+
+        [Test]
+        public async Task GetJobRoleViewModel_EncodedId_Is_Correct()
+        {
+            var result = await _orchestrator.GetJobRoleViewModel(new JobRoleRequest { EncodedAccountId = _encodedAccountId, CacheKey = _cacheKey });
+            Assert.AreEqual(_encodedAccountId, result.EncodedAccountId);
+        }
+
+        [Test]
+        public async Task GetJobRoleViewModel_CacheKey_Is_Correct()
+        {
+            var result = await _orchestrator.GetJobRoleViewModel(new JobRoleRequest { EncodedAccountId = _encodedAccountId, CacheKey = _cacheKey });
+            Assert.AreEqual(_cacheKey, result.CacheKey);
+        }
+
+        [Test]
+        public async Task GetJobRoleViewModel_JobRoles_Is_Correct()
+        {
+            var cacheItem = _fixture.Build<CreatePledgeCacheItem>()
+                    .With(x => x.JobRoles, TestHelper.GetRandomFlagsValue<JobRole>())
+                    .Create();
+
+            _cache.Setup(x => x.RetrieveFromCache<CreatePledgeCacheItem>(_cacheKey.ToString())).ReturnsAsync(cacheItem);
+
+            var result = await _orchestrator.GetJobRoleViewModel(new JobRoleRequest { EncodedAccountId = _encodedAccountId, CacheKey = _cacheKey });
+            Assert.AreEqual(cacheItem.JobRoles, result.JobRoles);
         }
     }
 }
