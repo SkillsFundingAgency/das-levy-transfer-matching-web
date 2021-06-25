dc84682ecccfbfa0ba29de1f5a789f769c8eebbb
James King <hello@jamesmichaelking.com>
2021-06-25 13:16:58 +0100

    Styles and JS for showing/hiding additional location fields

--- src/SFA.DAS.LevyTransferMatching.Web/wwwroot/js/app.js
+++ src/SFA.DAS.LevyTransferMatching.Web/wwwroot/js/app.js
@@ -1,4 +1,6 @@
-﻿function SelectAllCheckboxes(selectAllCheckboxes) {
+﻿// Select-all Checkboxes
+
+function SelectAllCheckboxes(selectAllCheckboxes) {
   this.checkBoxesWrap = selectAllCheckboxes;
   this.checkBoxes = this.checkBoxesWrap.querySelectorAll('input[type="checkbox"]');
   this.label = this.checkBoxesWrap.dataset.checkboxesSelectAllLabel || "Select all";
@@ -53,7 +55,151 @@ SelectAllCheckboxes.prototype.toggleCheckboxes = function (e) {
     }
 }
 
-const selectAllCheckboxes = document.querySelector('[data-checkboxes-select-all]')
+
+// Location Autocomplete
+
+var locationInputs = document.querySelectorAll(".app-location-autocomplete");
+var apiUrl = '/locations';
+
+if (locationInputs.length > 0) {
+
+  for (var i = 0; i < locationInputs.length; i++) {
+
+    var input = locationInputs[i]
+    var container = document.createElement('div');
+
+    container.className = "das-autocomplete-wrap"
+    input.parentNode.replaceChild(container, input);
+
+    var getSuggestions = function (query, updateResults) {
+      var results = [];
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          var jsonResponse = JSON.parse(xhr.responseText);
+          results = jsonResponse.locations.map(function (r) {
+            return r.name;
+          });
+          updateResults(results);
+        }
+      }
+      xhr.open("GET", apiUrl + '?searchTerm=' + query, true);
+      xhr.send();
+    };
+
+    accessibleAutocomplete({
+      element: container,
+      id: input.id,
+      name: input.name,
+      defaultValue: input.value,
+      displayMenu: 'overlay',
+      showNoOptionsFound: false,
+      minLength: 2,
+      source: getSuggestions,
+      placeholder: "",
+      confirmOnBlur: false,
+      autoselect: true
+    });
+  }
+}
+
+
+// Show/Hide Extra Location Fields
+
+function ExtraFieldRows(container) {
+
+  this.container = container
+  this.firstField = this.container.querySelector('input[type=text]')
+  this.fieldset = this.container.querySelector('.app-extra-fields__fieldset')
+  this.extraFieldRows = this.fieldset.querySelectorAll('.app-extra-fields__form-group')
+  this.allLocations = this.container.querySelector('.app-extra-fields__all-locations')
+  this.hiddenClass = 'app-extra-field__form-group--hidden'
+
+  this.addLink()
+  this.allLocationsCheckboxEvent()
+
+  this.firstField.addEventListener('change', this.clearAllLocationsCheckbox.bind(this))
+
+  for (var f = 0; f < this.extraFieldRows.length; f++) {
+    var extraFieldRow = this.extraFieldRows[f]
+    var textInput = extraFieldRow.querySelector('input[type=text]')
+    this.addRemoveLink(extraFieldRow)
+    if (textInput.value === '') {
+      this.hideRow(extraFieldRow)
+    }
+  }
+}
+
+ExtraFieldRows.prototype.addLink = function () {
+  var addRowLink = document.createElement('a');
+  addRowLink.innerHTML = 'Add another location'
+  addRowLink.className = 'govuk-link govuk-link--no-visited-state app-extra-field__form-group-link--add'
+  addRowLink.href = '#'
+  addRowLink.addEventListener('click', this.showFirstAvailableRow.bind(this))
+  this.fieldset.parentNode.insertBefore(addRowLink, this.fieldset.nextSibling);
+}
+
+ExtraFieldRows.prototype.showFirstAvailableRow = function () {
+  for (var f = 0; f < this.extraFieldRows.length; f++) {
+    var extraFieldRow = this.extraFieldRows[f]
+    if (extraFieldRow.classList.contains(this.hiddenClass)) {
+      this.showRow(extraFieldRow)
+      break;
+    }
+  }
+}
+
+ExtraFieldRows.prototype.allLocationsCheckboxEvent = function () {
+  var that = this
+  if (this.allLocations) {
+    this.allLocations.addEventListener('click', function () {
+      if (this.checked) {
+        var firstField = that.container.querySelector('input[type=text]')
+        firstField.value = ""
+        for (var f = 0; f < that.extraFieldRows.length; f++) {
+          var extraFieldRow = that.extraFieldRows[f]
+          that.hideRow(extraFieldRow)
+        }
+      }
+    })
+  }
+}
+
+ExtraFieldRows.prototype.clearAllLocationsCheckbox = function () {
+  this.allLocations.checked = false
+}
+
+ExtraFieldRows.prototype.addRemoveLink = function (row) {
+  var that = this
+  var removeLink = document.createElement('a');
+  removeLink.innerHTML = 'Remove'
+  removeLink.className = 'govuk-link govuk-link--no-visited-state app-extra-field__form-group-link--remove'
+  removeLink.href = "#"
+  removeLink.addEventListener('click', function(){ that.hideRow(row); })
+  row.append(removeLink)
+}
+
+ExtraFieldRows.prototype.hideRow = function (row) {
+  var textInput = row.querySelector('input[type=text]')
+  textInput.value = ''
+  row.classList.add(this.hiddenClass)
+}
+
+ExtraFieldRows.prototype.showRow = function (row) {
+  this.clearAllLocationsCheckbox()
+  row.classList.remove(this.hiddenClass)
+}
+
+
+
+// App 
+
+var selectAllCheckboxes = document.querySelector('[data-checkboxes-select-all]')
 if (selectAllCheckboxes) {
   new SelectAllCheckboxes(selectAllCheckboxes);
+}
+
+var extraFieldRows = document.querySelector('[data-extra-field-rows]')
+if (extraFieldRows) {
+  new ExtraFieldRows(extraFieldRows);
 }
