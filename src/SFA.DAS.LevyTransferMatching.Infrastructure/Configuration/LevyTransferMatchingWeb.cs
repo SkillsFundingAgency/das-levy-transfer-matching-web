05a8364de85d81ff3b4e3603974bbc7ee8a2fd98
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-06-28 13:16:02 +0100

    Ability to override the UtcNow property on the provider, for testing purposes

--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Configuration/LevyTransferMatchingWeb.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/Configuration/LevyTransferMatchingWeb.cs
@@ -1,8 +1,11 @@
-﻿namespace SFA.DAS.LevyTransferMatching.Infrastructure.Configuration
+﻿using System;
+
+namespace SFA.DAS.LevyTransferMatching.Infrastructure.Configuration
 {
     public class LevyTransferMatchingWeb
     {
         public string RedisConnectionString { get; set; }
         public string DataProtectionKeysDatabase { get; set; }
+        public DateTime? UtcNowOverride { get; set; }
     }
 }
--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/DateTimeService/DateTimeService.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/DateTimeService/DateTimeService.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿using SFA.DAS.LevyTransferMatching.Infrastructure.Configuration;
+using System;
 
 namespace SFA.DAS.LevyTransferMatching.Infrastructure.Services.DateTimeService
 {
     public class DateTimeService : IDateTimeService
     {
-        public DateTime UtcNow => DateTime.UtcNow;
+        private readonly LevyTransferMatchingWeb _levyTransferMatchingWeb;
+
+        public DateTimeService(LevyTransferMatchingWeb levyTransferMatchingWeb)
+        {
+            _levyTransferMatchingWeb = levyTransferMatchingWeb;
+        }
+
+        public DateTime UtcNow
+        {
+            get
+            {
+                if (_levyTransferMatchingWeb.UtcNowOverride.HasValue)
+                {
+                    return _levyTransferMatchingWeb.UtcNowOverride.Value;
+                }
+                else
+                {
+                    return DateTime.UtcNow;
+                }
+            }
+        }
     }
 }
