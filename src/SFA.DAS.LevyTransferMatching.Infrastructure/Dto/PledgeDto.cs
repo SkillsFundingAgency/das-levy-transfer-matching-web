52aaefab76bebd425bed9be839aa6e2a037382b8
dannygb <dan.beavon@education.gov.uk>
2021-06-29 12:19:19 +0100

    [TM-77] - Removes the id property from the pledge dto now it is not used

--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Dto/PledgeDto.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/Dto/PledgeDto.cs
@@ -4,7 +4,6 @@ namespace SFA.DAS.LevyTransferMatching.Infrastructure.Dto
 {
     public class PledgeDto
     {
-        public string Id { get; set; }
         public int Amount { get; set; }
         public bool IsNamePublic { get; set; }
         public List<string> Sectors { get; set; }
