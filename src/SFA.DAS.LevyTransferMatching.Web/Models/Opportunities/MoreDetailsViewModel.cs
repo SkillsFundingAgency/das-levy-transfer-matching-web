835fe645fa011d32e8082c4bdeecad69e642d3ce
dannygb <dan.beavon@education.gov.uk>
2021-07-05 09:12:52 +0100

    [TM-39] - Correct SonarCloud issues

--- src/SFA.DAS.LevyTransferMatching.Web/Models/Opportunities/MoreDetailsViewModel.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Models/Opportunities/MoreDetailsViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using SFA.DAS.LevyTransferMatching.Web.Models.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.LevyTransferMatching.Web.Models.Opportunities
 {
