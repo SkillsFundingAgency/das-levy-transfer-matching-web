ae105f4a0fdf78b010dcbadb4c968d305043721c
dannygb <dan.beavon@education.gov.uk>
2021-07-05 09:35:34 +0100

    [TM-37] - Clears SonarCloud issues

--- src/SFA.DAS.LevyTransferMatching.Web/Models/Opportunities/ApplicationRequest.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Models/Opportunities/ApplicationRequest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SFA.DAS.LevyTransferMatching.Web.Models.Opportunities
+﻿namespace SFA.DAS.LevyTransferMatching.Web.Models.Opportunities
 {
     public class ApplicationRequest
     {
