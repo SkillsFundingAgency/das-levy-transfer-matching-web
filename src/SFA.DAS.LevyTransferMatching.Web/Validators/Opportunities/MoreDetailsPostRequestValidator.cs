a355aad614ffc75ee5823ac77ee6425a2b495cad
dannygb <dan.beavon@education.gov.uk>
2021-07-21 16:00:30 +0100

    [TM-39] - Modify the word count algorithm to ignore empty strings in the count

--- src/SFA.DAS.LevyTransferMatching.Web/Validators/Opportunities/MoreDetailsPostRequestValidator.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Validators/Opportunities/MoreDetailsPostRequestValidator.cs
@@ -7,12 +7,13 @@ namespace SFA.DAS.LevyTransferMatching.Web.Validators.Opportunities
     public class MoreDetailsPostRequestValidator : AbstractValidator<MoreDetailsPostRequest>
     {
         private const int MaxWords = 200;
+        private const string Space = " ";
 
         public MoreDetailsPostRequestValidator()
         {
             RuleFor(x => x.Details)
-                .Must(details => details.Replace("\r\n", " ").Split(" ").Count() <= MaxWords)
-                .When(x => x.Details != null && x.Details.Length > 0)
+                .Must(details => details.Replace("\r\n", Space).Split(Space).Count(x => !string.IsNullOrEmpty(x)) <= MaxWords)
+                .When(x => !string.IsNullOrEmpty(x.Details))
                 .WithMessage("The details entered must be less than 200 words");
         }
     }
