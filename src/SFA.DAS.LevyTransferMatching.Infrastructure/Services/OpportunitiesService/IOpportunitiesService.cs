61e617ba7b012c553f3d1503efcc535c0c102761
Conor Till <conortill@gmail.com>
2021-07-30 10:59:38 +0100

    Removing GetAllOpportunities

--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/OpportunitiesService/IOpportunitiesService.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/OpportunitiesService/IOpportunitiesService.cs
@@ -7,8 +7,6 @@ namespace SFA.DAS.LevyTransferMatching.Infrastructure.Services.OpportunitiesServ
 {
     public interface IOpportunitiesService
     {
-        Task<List<OpportunityDto>> GetAllOpportunities();
-
         Task<OpportunityDto> GetOpportunity(int id);
         Task<GetIndexResponse> GetIndex();
         Task<ApplicationDetailsDto> GetApplicationDetails(int id);
