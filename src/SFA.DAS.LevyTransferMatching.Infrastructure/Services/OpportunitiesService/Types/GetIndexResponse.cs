89f1189f0ee6f0c61a8ffe6e407899fab3d42574
Conor Till <conortill@gmail.com>
2021-07-30 09:31:42 +0100

    Removing use of OpportunityDto

--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/OpportunitiesService/Types/GetIndexResponse.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/OpportunitiesService/Types/GetIndexResponse.cs
@@ -6,9 +6,21 @@ namespace SFA.DAS.LevyTransferMatching.Infrastructure.Services.OpportunitiesServ
 {
     public class GetIndexResponse
     {
-        public List<OpportunityDto> Opportunities { get; set; }
+        public List<Opportunity> Opportunities { get; set; }
         public List<ReferenceDataItem> Levels { get; set; }
         public List<ReferenceDataItem> Sectors { get; set; }
         public List<ReferenceDataItem> JobRoles { get; set; }
+
+        public class Opportunity
+        {
+            public int Id { get; set; }
+            public string DasAccountName { get; set; }
+            public IEnumerable<string> JobRoles { get; set; }
+            public IEnumerable<string> Levels { get; set; }
+            public IEnumerable<string> Sectors { get; set; }
+            public IEnumerable<string> Locations { get; set; }
+            public int Amount { get; set; }
+            public bool IsNamePublic { get; set; }
+        }
     }
 }
