a320e694b6d8f2cf0b80452382009fa05526790e
chrisfoster76 <chrisfoster186@googlemail.com>
2021-09-10 11:15:07 +0100

    Removes unused using

--- src/SFA.DAS.LevyTransferMatching.Web/Orchestrators/PledgeOrchestrator.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Orchestrators/PledgeOrchestrator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Threading;
 using SFA.DAS.Encoding;
