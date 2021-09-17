52806d9e724303fffd66467806b08b49cbf8ab0a
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-09-17 12:17:39 +0100

    Comment describing the need for the IValidatorInteceptor.

--- src/SFA.DAS.LevyTransferMatching.Web/ValidatorInterceptors/LocationSelectPostRequestValidatorInterceptor.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/ValidatorInterceptors/LocationSelectPostRequestValidatorInterceptor.cs
@@ -7,6 +7,18 @@ using SFA.DAS.LevyTransferMatching.Web.Models.Pledges;
 
 namespace SFA.DAS.LevyTransferMatching.Web.ValidatorInterceptors
 {
+    /// <summary>
+    /// This interceptor exists because there seems to be an underlying issue
+    /// with either the ASP.NET MVC validation or Fluent; (valid) ModelState
+    /// entries were not being generated for this validated structure, and
+    /// as a result, the ValidationSummary partial wasn't rendering out links
+    /// in certain situations, due to the way it loops through on a consecutive
+    /// index.
+    /// This fix ensures there's always ModelState entries for each value in
+    /// the
+    /// <see cref="LocationSelectPostRequest.SelectValidLocationGroups" />
+    /// property.
+    /// </summary>
     public class LocationSelectPostRequestValidatorInterceptor : IValidatorInterceptor
     {
         public ValidationResult AfterAspNetValidation(ActionContext actionContext, IValidationContext validationContext, ValidationResult result)
