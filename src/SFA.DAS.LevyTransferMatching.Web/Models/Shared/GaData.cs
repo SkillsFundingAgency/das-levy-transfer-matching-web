b50ccb6839b8de8af8eacaa734b7b33e42e2c6b9
Chris Woodcock <chris@humesoftware.com>
2022-11-28 17:02:04 +0000

    Added google analytics tags

---
+++ src/SFA.DAS.LevyTransferMatching.Web/Models/Shared/GaData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.LevyTransferMatching.Web.Models.Shared
+{
+    public class GaData
+    {
+        public string DataLoaded { get; set; } = "dataLoaded";
+        public string UserId { get; set; }
+        public string Vpv { get; set; }
+        public string Acc { get; set; }
+        public string Org { get; set; }
+        public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
+    }
+}
