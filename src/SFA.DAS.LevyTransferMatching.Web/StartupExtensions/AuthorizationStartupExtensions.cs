7fdff569b877a846d67775fd1eb8e30afa327e9a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-04-11 19:23:20 +0100

    FAI-835 - Added filter for account suspended
    When this is upgraded to .net6 the filter can be deleted, however in 3.1 its not possible to redirect in the context of the authhandler

--- src/SFA.DAS.LevyTransferMatching.Web/StartupExtensions/AuthorizationStartupExtensions.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/StartupExtensions/AuthorizationStartupExtensions.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.GovUK.Auth.Authentication;
+using SFA.DAS.GovUK.Auth.Extensions;
 using SFA.DAS.LevyTransferMatching.Infrastructure.Configuration;
 using SFA.DAS.LevyTransferMatching.Web.Authentication;
 
@@ -30,6 +36,32 @@ namespace SFA.DAS.LevyTransferMatching.Web.StartupExtensions
             });
 
             return services;
+        }
+    }
+
+    //TODO once upgraded to .net6 - this filter can be deleted
+    public class AccountActiveFilter : IActionFilter
+    {
+        private readonly IConfiguration _configuration;
+
+        public AccountActiveFilter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context != null)
+            {
+                var isAccountSuspended = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.AuthorizationDecision))?.Value;
+                if (isAccountSuspended != null && isAccountSuspended.Equals("Suspended", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    context.HttpContext.Response.Redirect(RedirectExtension.GetAccountSuspendedRedirectUrl(_configuration["ResourceEnvironmentName"]));
+                }
+            }
         }
     }
 }
