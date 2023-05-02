75b00c5cded2318a02563a79c8e35c0e34aa00f1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-05-02 17:52:46 +0100

    FAI-724 - Change user service to check new claim values

--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Configuration/ClaimIdentifierConfiguration.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/Configuration/ClaimIdentifierConfiguration.cs
@@ -2,15 +2,8 @@
 {
     public class ClaimIdentifierConfiguration
     {
-        public const string ClaimsBaseUrl = "";
         public const string Id = "http://das/employer/identity/claims/id";
-        public const string GivenName = "http://das/employer/identity/claims/given_name";
-        public const string FamilyName = "http://das/employer/identity/claims/family_name";
-        public const string Email = "http://das/employer/identity/claims/email_address";
         public const string DisplayName = "http://das/employer/identity/claims/display_name";
         public const string Account = "http://das/employer/identity/claims/account";
-        public const string AccountOwner = "http://das/employer/identity/claims/account_owner";
-        public const string AccountTransactor = "http://das/employer/identity/claims/account_transactor";
-        public const string AccountViewer = "http://das/employer/identity/claims/account_viewer";
     }
 }
