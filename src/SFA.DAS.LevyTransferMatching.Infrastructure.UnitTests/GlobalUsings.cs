9f0f2c880b68b0ab0203e5b07d5bbdc1b8584357
Jon Harris <jon.harris@digital.education.gov.uk>
2023-11-30 15:41:07 +0000

    Implementing GlobalUsings.cs

---
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure.UnitTests/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿// Global using directives
+
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Linq.Expressions;
+global using System.Net;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using AutoFixture;
+global using FluentAssertions;
+global using Moq;
+global using Moq.Protected;
+global using Newtonsoft.Json;
+global using NUnit.Framework;
--- src/SFA.DAS.LevyTransferMatching.Infrastructure.UnitTests/Services/EmployerAccountsServiceTests.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure.UnitTests/Services/EmployerAccountsServiceTests.cs
@@ -1,129 +1,119 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Azure.Documents.Client;
+﻿using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Documents.Linq;
-using Moq;
-using NUnit.Framework;
 using SFA.DAS.LevyTransferMatching.Infrastructure.Services.AccountUsersReadStore;
 using SFA.DAS.LevyTransferMatching.Infrastructure.Services.EmployerAccountsService;
 using SFA.DAS.LevyTransferMatching.Infrastructure.Services.EmployerAccountsService.Types;
 
-namespace SFA.DAS.LevyTransferMatching.Infrastructure.UnitTests.Services
+namespace SFA.DAS.LevyTransferMatching.Infrastructure.UnitTests.Services;
+
+[TestFixture]
+public class EmployerAccountsServiceTests
 {
-    [TestFixture]
-    public class EmployerAccountsServiceTests
+    private IEmployerAccountsService _employerAccountsApiClient;
+    private Mock<IAccountUsersReadOnlyRepository> _mockAccountUsersReadOnlyRepository;
+    private readonly Guid _userRef = Guid.NewGuid();
+
+    [SetUp]
+    public void Setup()
     {
-        private IEmployerAccountsService _employerAccountsApiClient;
-        private Mock<IAccountUsersReadOnlyRepository> _mockAccountUsersReadOnlyRepository;
-        private readonly Guid _userRef = Guid.NewGuid();
+        _mockAccountUsersReadOnlyRepository = new Mock<IAccountUsersReadOnlyRepository>();
 
-        [SetUp]
-        public void Setup()
+        var accountUsers = new List<Infrastructure.Services.AccountUsersReadStore.Types.AccountUser>();
+        accountUsers.Add(new Infrastructure.Services.AccountUsersReadStore.Types.AccountUser
         {
-            _mockAccountUsersReadOnlyRepository = new Mock<IAccountUsersReadOnlyRepository>();
+            accountId = 123, ETag = "", Id = Guid.NewGuid(), role = UserRole.Owner, removed = default , userRef = _userRef
+        });
 
-            var accountUsers = new List<Infrastructure.Services.AccountUsersReadStore.Types.AccountUser>();
-            accountUsers.Add(new Infrastructure.Services.AccountUsersReadStore.Types.AccountUser
-            {
-                accountId = 123, ETag = "", Id = Guid.NewGuid(), role = UserRole.Owner, removed = default , userRef = _userRef
-            });
+        var documentQuery = new FakeDocumentQuery<Infrastructure.Services.AccountUsersReadStore.Types.AccountUser>(accountUsers);
 
-            var documentQuery = new FakeDocumentQuery<Infrastructure.Services.AccountUsersReadStore.Types.AccountUser>(accountUsers);
+        _mockAccountUsersReadOnlyRepository.Setup(m =>
+                m.CreateQuery(It.IsAny<FeedOptions>()))
+            .Returns(documentQuery);
 
-            _mockAccountUsersReadOnlyRepository.Setup(m =>
-                    m.CreateQuery(It.IsAny<FeedOptions>()))
-                .Returns(documentQuery);
+        _employerAccountsApiClient = new EmployerAccountsService(_mockAccountUsersReadOnlyRepository.Object);
+    }
 
-            _employerAccountsApiClient = new EmployerAccountsService(_mockAccountUsersReadOnlyRepository.Object);
-        }
-
-        [Test]
-        public async Task IsUserInRole_Returns_True_If_User_Is_In_Specified_Role()
-        {
-            var roles = new HashSet<UserRole> {UserRole.Owner};
-            var result = await _employerAccountsApiClient.IsUserInRole(_userRef, 123, roles, CancellationToken.None);
-            Assert.IsTrue(result);
-        }
+    [Test]
+    public async Task IsUserInRole_Returns_True_If_User_Is_In_Specified_Role()
+    {
+        var roles = new HashSet<UserRole> {UserRole.Owner};
+        var result = await _employerAccountsApiClient.IsUserInRole(_userRef, 123, roles, CancellationToken.None);
+        Assert.IsTrue(result);
+    }
 
 #pragma warning disable S3881 // "IDisposable" should be implemented correctly
-        private class FakeDocumentQuery<T> : IDocumentQuery<T>, IOrderedQueryable<T>
+    private class FakeDocumentQuery<T> : IDocumentQuery<T>, IOrderedQueryable<T>
 #pragma warning restore S3881 // "IDisposable" should be implemented correctly
+    {
+        public Expression Expression => _query.Expression;
+        public Type ElementType => _query.ElementType;
+        public bool HasMoreResults => ++_page <= _pages;
+        public IQueryProvider Provider => new FakeDocumentQueryProvider<T>(_query.Provider);
+
+        private readonly IQueryable<T> _query;
+        private readonly int _pages = 1;
+        private int _page = 0;
+
+        public FakeDocumentQuery(IEnumerable<T> data)
         {
-            public Expression Expression => _query.Expression;
-            public Type ElementType => _query.ElementType;
-            public bool HasMoreResults => ++_page <= _pages;
-            public IQueryProvider Provider => new FakeDocumentQueryProvider<T>(_query.Provider);
+            _query = data.AsQueryable();
+        }
 
-            private readonly IQueryable<T> _query;
-            private readonly int _pages = 1;
-            private int _page = 0;
+        public Task<FeedResponse<TResult>> ExecuteNextAsync<TResult>(CancellationToken token = default)
+        {
+            return Task.FromResult(new FeedResponse<TResult>(this.Cast<TResult>()));
+        }
 
-            public FakeDocumentQuery(IEnumerable<T> data)
-            {
-                _query = data.AsQueryable();
-            }
+        public Task<FeedResponse<dynamic>> ExecuteNextAsync(CancellationToken token = default)
+        {
+            return Task.FromResult(new FeedResponse<dynamic>(this.Cast<dynamic>()));
+        }
 
-            public Task<FeedResponse<TResult>> ExecuteNextAsync<TResult>(CancellationToken token = default)
-            {
-                return Task.FromResult(new FeedResponse<TResult>(this.Cast<TResult>()));
-            }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _query.GetEnumerator();
+        }
 
-            public Task<FeedResponse<dynamic>> ExecuteNextAsync(CancellationToken token = default)
-            {
-                return Task.FromResult(new FeedResponse<dynamic>(this.Cast<dynamic>()));
-            }
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                return _query.GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
 
 #pragma warning disable S1186 // Methods should not be empty
-            public void Dispose()
+        public void Dispose()
 #pragma warning restore S1186 // Methods should not be empty
-            {
-            }
-        }
-
-        private class FakeDocumentQueryProvider<T> : IQueryProvider
         {
-            private readonly IQueryProvider _queryProvider;
+        }
+    }
 
-            public FakeDocumentQueryProvider(IQueryProvider queryProvider)
-            {
-                _queryProvider = queryProvider;
-            }
+    private class FakeDocumentQueryProvider<T> : IQueryProvider
+    {
+        private readonly IQueryProvider _queryProvider;
 
-            public IQueryable CreateQuery(Expression expression)
-            {
-                return new FakeDocumentQuery<T>(_queryProvider.CreateQuery<T>(expression));
-            }
-
-            public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
-            {
-                return new FakeDocumentQuery<TElement>(_queryProvider.CreateQuery<TElement>(expression));
-            }
-
-            public object Execute(Expression expression)
-            {
-                return _queryProvider.Execute(expression);
-            }
-
-            public TResult Execute<TResult>(Expression expression)
-            {
-                return _queryProvider.Execute<TResult>(expression);
-            }
+        public FakeDocumentQueryProvider(IQueryProvider queryProvider)
+        {
+            _queryProvider = queryProvider;
         }
 
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new FakeDocumentQuery<T>(_queryProvider.CreateQuery<T>(expression));
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new FakeDocumentQuery<TElement>(_queryProvider.CreateQuery<TElement>(expression));
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _queryProvider.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _queryProvider.Execute<TResult>(expression);
+        }
     }
+
 }
