f6c9e6d1a9434d081d7dea36ec2a42b1a5de3020
Jon Harris <jon.harris@digital.education.gov.uk>
2023-12-04 10:23:53 +0000

    refactor

--- src/SFA.DAS.LevyTransferMatching.Web/Attributes/EnableGoogleAnalyticsAttribute.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Attributes/EnableGoogleAnalyticsAttribute.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using SFA.DAS.LevyTransferMatching.Infrastructure.Configuration;
@@ -9,23 +8,29 @@ namespace SFA.DAS.LevyTransferMatching.Web.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class EnableGoogleAnalyticsAttribute : ResultFilterAttribute
     {
-        public GoogleAnalytics GoogleAnalyticsConfiguration { get; }
+        private readonly GoogleAnalytics _googleAnalyticsConfiguration;
 
         public EnableGoogleAnalyticsAttribute(GoogleAnalytics configuration)
         {
-            GoogleAnalyticsConfiguration = configuration;
+            _googleAnalyticsConfiguration = configuration;
         }
 
         public override void OnResultExecuting(ResultExecutingContext context)
         {
-            if (context.Controller is PageModel page)
-                SetViewData(page.ViewData);
+            switch (context.Controller)
+            {
+                case PageModel page:
+                    SetViewData(page.ViewData);
+                    break;
+                case Controller controller:
+                    SetViewData(controller.ViewData);
+                    break;
+            }
 
-            if (context.Controller is Controller controller)
-                SetViewData(controller.ViewData);
+            return;
 
             void SetViewData(ViewDataDictionary viewData)
-                => viewData[ViewDataKeys.GoogleAnalyticsConfiguration] = GoogleAnalyticsConfiguration;
+                => viewData[ViewDataKeys.GoogleAnalyticsConfiguration] = _googleAnalyticsConfiguration;
         }
     }
 }
