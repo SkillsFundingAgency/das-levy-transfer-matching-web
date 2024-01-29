49751f580f20249cc07cc9a30a9b3b3d1855b92e
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2024-01-29 20:26:34 +0000

    remove all cosmos accounts readstore references

--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/AccountUsersReadStore/IAccountUsersReadOnlyRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.CosmosDb;
-using SFA.DAS.LevyTransferMatching.Infrastructure.Services.AccountUsersReadStore.Types;
-
-namespace SFA.DAS.LevyTransferMatching.Infrastructure.Services.AccountUsersReadStore
-{
-    public interface IAccountUsersReadOnlyRepository : IReadOnlyDocumentRepository<AccountUser>
-    {
-    }
-}
--- src/SFA.DAS.LevyTransferMatching.Infrastructure/Services/EmployerAccountsService/Types/UserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.LevyTransferMatching.Infrastructure.Services.EmployerAccountsService.Types
-{
-    public enum UserRole
-    {
-        Viewer = 3,
-        Transactor = 2,
-        Owner = 1
-    }
-}
