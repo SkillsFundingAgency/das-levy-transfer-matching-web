3a947c45b62a0775b882c2946e8833bd0c785c73
ogrwill <will.ogrady@digital.education.gov.uk>
2024-11-14 14:40:59 +0000

    update rule for csp

--- src/SFA.DAS.LevyTransferMatching.Web/StartupExtensions/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/StartupExtensions/SecurityHeadersMiddleware.cs
@@ -24,7 +24,7 @@ public class SecurityHeadersMiddleware
                 "https://das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net https://das-pp-frnt-end.azureedge.net https://das-test-frnt-end.azureedge.net https://das-at-frnt-end.azureedge.net " +
                 "*.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com; " +
                 $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io ; " +
-                $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://*.test2-eas.apprenticeships.education.gov.uk ; " +
+                $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://*eas.apprenticeships.education.gov.uk ; " +
                 $"font-src {dasCdn} https://fonts.gstatic.com https://*.rcrsv.io data: ;" +
                 "connect-src 'self' https://*.google-analytics.com https://*.zendesk.com https://*.zdassets.com wss://*.zopim.com https://*.rcrsv.io ;"));
 
