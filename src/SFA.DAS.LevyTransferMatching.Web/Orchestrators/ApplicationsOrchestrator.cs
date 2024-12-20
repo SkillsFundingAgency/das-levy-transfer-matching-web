3fcb0ed65802785b90e9527197c75ac5c4969d52
ogrwill <will.ogrady@digital.education.gov.uk>
2024-12-20 14:34:02 +0000

    updating content of accepted page

--- src/SFA.DAS.LevyTransferMatching.Web/Orchestrators/ApplicationsOrchestrator.cs
+++ src/SFA.DAS.LevyTransferMatching.Web/Orchestrators/ApplicationsOrchestrator.cs
@@ -134,7 +134,10 @@ public class ApplicationsOrchestrator : OpportunitiesOrchestratorBase, IApplicat
         {
             EncodedAccountId = request.EncodedAccountId,
             EncodedApplicationId = request.EncodedApplicationId,
-            EmployerNameAndReference = $"{result.EmployerAccountName} ({encodedPledgeId})",
+            //EmployerNameAndReference = $"{result.EmployerAccountName} ({encodedPledgeId})",
+            EmployerName = result.EmployerAccountName,
+            PledgeReference = encodedPledgeId
+
         };
     }
 
