0947cb8b949ce96f880405264c73c1bfba08a188
Jon Harris <jon.harris@fuitsu.com>
2025-01-06 12:03:46 +0000

    Tidying/minor refactorin

--- src/SFA.DAS.LevyTransferMatching.Domain/Extensions/ApplicationStatusExtensions.cs
+++ src/SFA.DAS.LevyTransferMatching.Domain/Extensions/ApplicationStatusExtensions.cs
@@ -1,102 +1,99 @@
 ﻿using System;
 using SFA.DAS.LevyTransferMatching.Domain.Types;
 
-namespace SFA.DAS.LevyTransferMatching.Domain.Extensions
+namespace SFA.DAS.LevyTransferMatching.Domain.Extensions;
+
+public static class ApplicationStatusExtensions
 {
-    public static class ApplicationStatusExtensions
+    public static string GetLabelForSender(this ApplicationStatus status, int? remainingDaysForDelayedApproval, int? remainingDaysForAutoRejection)
     {
-        public static string GetLabelForSender(this ApplicationStatus status, int? RemainingDaysForDelayedApproval, int? RemainingDaysForAutoRejection)
+        if (remainingDaysForDelayedApproval.HasValue)
         {
-            if (RemainingDaysForDelayedApproval.HasValue)
-            {
-                string autoApprovalDate = GetAutoApprovalDate(RemainingDaysForDelayedApproval.Value);
-                return $"AUTO APPROVAL ON {autoApprovalDate}";
-            }
-
-            if (RemainingDaysForAutoRejection.HasValue)
-            {
-                string autoApprovalDate = GetAutoApprovalDate(RemainingDaysForAutoRejection.Value);
-                return $"APPLICATION EXPIRES ON {autoApprovalDate}";
-            }
-
-            switch (status)
-            {
-                case ApplicationStatus.Pending: return "AWAITING YOUR APPROVAL";
-                case ApplicationStatus.Approved: return "AWAITING ACCEPTANCE BY APPLICANT";
-                case ApplicationStatus.Accepted: return "OFFER OF FUNDING ACCEPTED";
-                case ApplicationStatus.FundsUsed: return "FUNDS USED";
-                case ApplicationStatus.Rejected: return "REJECTED";
-                case ApplicationStatus.Declined: return "WITHDRAWN BY APPLICANT";
-                case ApplicationStatus.Withdrawn: return "WITHDRAWN BY APPLICANT";
-                case ApplicationStatus.WithdrawnAfterAcceptance: return "WITHDRAWN BY APPLICANT";
-                default:
-                    return string.Empty;
-            }
+            var autoApprovalDate = GetAutoApprovalDate(remainingDaysForDelayedApproval.Value);
+            return $"AUTO APPROVAL ON {autoApprovalDate}";
         }
 
-        private static string GetAutoApprovalDate(int remainingDays)
+        if (remainingDaysForAutoRejection.HasValue)
         {
-            DateTime futureDate = DateTime.Today.AddDays(remainingDays);
-            string formattedDate = futureDate.ToString("dd MMM yyyy").ToUpper();
-            return formattedDate;
+            var autoApprovalDate = GetAutoApprovalDate(remainingDaysForAutoRejection.Value);
+            return $"APPLICATION EXPIRES ON {autoApprovalDate}";
         }
 
-        public static string GetCssClassForSender(this ApplicationStatus status, int? RemainingDaysForDelayedApproval, int? RemainingDaysForAutoRejection)
+        return status switch
         {
-            if (RemainingDaysForDelayedApproval.HasValue)
-            {
-                return "govuk-tag govuk-tag--yellow";
-            }
+            ApplicationStatus.Pending => "AWAITING YOUR APPROVAL",
+            ApplicationStatus.Approved => "AWAITING ACCEPTANCE BY APPLICANT",
+            ApplicationStatus.Accepted => "OFFER OF FUNDING ACCEPTED",
+            ApplicationStatus.FundsUsed => "FUNDS USED",
+            ApplicationStatus.Rejected => "REJECTED",
+            ApplicationStatus.Declined => "WITHDRAWN BY APPLICANT",
+            ApplicationStatus.Withdrawn => "WITHDRAWN BY APPLICANT",
+            ApplicationStatus.WithdrawnAfterAcceptance => "WITHDRAWN BY APPLICANT",
+            _ => string.Empty
+        };
+    }
 
-            if (RemainingDaysForAutoRejection.HasValue)
-            {
-                return "govuk-tag govuk-tag--orange";
-            }
-            switch (status)
-            {
-                case ApplicationStatus.Pending: return "govuk-tag govuk-tag--blue";
-                case ApplicationStatus.Approved: return "govuk-tag govuk-tag--yellow";
-                case ApplicationStatus.Accepted: return "govuk-tag govuk-tag--turquoise";
-                case ApplicationStatus.FundsUsed: return "govuk-tag govuk-tag--pink";
-                case ApplicationStatus.Rejected: return "govuk-tag govuk-tag--red";
-                case ApplicationStatus.Declined: return "govuk-tag govuk-tag--yellow";
-                case ApplicationStatus.Withdrawn: return "govuk-tag govuk-tag--yellow";
-                case ApplicationStatus.WithdrawnAfterAcceptance: return "govuk-tag govuk-tag--yellow";
-                default:
-                    return string.Empty;
-            }
-        }       
-        public static string GetLabelForReceiver(this ApplicationStatus status)
+    private static string GetAutoApprovalDate(int remainingDays)
+    {
+        var futureDate = DateTime.Today.AddDays(remainingDays);
+        var formattedDate = futureDate.ToString("dd MMM yyyy").ToUpper();
+        return formattedDate;
+    }
+
+    public static string GetCssClassForSender(this ApplicationStatus status, int? remainingDaysForDelayedApproval, int? remainingDaysForAutoRejection)
+    {
+        if (remainingDaysForDelayedApproval.HasValue)
         {
-            return status switch
-            {
-                ApplicationStatus.Pending => "Awaiting approval",
-                ApplicationStatus.Approved => "Approved, awaiting your acceptance",
-                ApplicationStatus.Rejected => "Rejected",
-                ApplicationStatus.Accepted => "Funds available to add apprentice",
-                ApplicationStatus.FundsUsed => "Funds used",
-                ApplicationStatus.Declined => "Withdrawn",
-                ApplicationStatus.Withdrawn => "Withdrawn",
-                ApplicationStatus.WithdrawnAfterAcceptance => "Withdrawn",
-                _ => string.Empty,
-            };
+            return "govuk-tag govuk-tag--yellow";
         }
 
-        public static string GetCssClassForReceiver(this ApplicationStatus status)
+        if (remainingDaysForAutoRejection.HasValue)
         {
-            switch (status)
-            {
-                case ApplicationStatus.Pending: return "govuk-tag govuk-tag--grey";
-                case ApplicationStatus.Approved: return "govuk-tag govuk-tag--blue";
-                case ApplicationStatus.Rejected: return "govuk-tag govuk-tag--red";
-                case ApplicationStatus.Accepted: return "govuk-tag govuk-tag--turquoise";
-                case ApplicationStatus.FundsUsed: return "govuk-tag govuk-tag--pink";
-                case ApplicationStatus.Declined: return "govuk-tag govuk-tag--yellow";
-                case ApplicationStatus.Withdrawn: return "govuk-tag govuk-tag--yellow";
-                case ApplicationStatus.WithdrawnAfterAcceptance: return "govuk-tag govuk-tag--yellow";
-                default:
-                    return string.Empty;
-            }
+            return "govuk-tag govuk-tag--orange";
         }
+
+        return status switch
+        {
+            ApplicationStatus.Pending => "govuk-tag govuk-tag--blue",
+            ApplicationStatus.Approved => "govuk-tag govuk-tag--yellow",
+            ApplicationStatus.Accepted => "govuk-tag govuk-tag--turquoise",
+            ApplicationStatus.FundsUsed => "govuk-tag govuk-tag--pink",
+            ApplicationStatus.Rejected => "govuk-tag govuk-tag--red",
+            ApplicationStatus.Declined => "govuk-tag govuk-tag--yellow",
+            ApplicationStatus.Withdrawn => "govuk-tag govuk-tag--yellow",
+            ApplicationStatus.WithdrawnAfterAcceptance => "govuk-tag govuk-tag--yellow",
+            _ => string.Empty
+        };
+    }       
+    public static string GetLabelForReceiver(this ApplicationStatus status)
+    {
+        return status switch
+        {
+            ApplicationStatus.Pending => "Awaiting approval",
+            ApplicationStatus.Approved => "Approved, awaiting your acceptance",
+            ApplicationStatus.Rejected => "Rejected",
+            ApplicationStatus.Accepted => "Funds available to add apprentice",
+            ApplicationStatus.FundsUsed => "Funds used",
+            ApplicationStatus.Declined => "Withdrawn",
+            ApplicationStatus.Withdrawn => "Withdrawn",
+            ApplicationStatus.WithdrawnAfterAcceptance => "Withdrawn",
+            _ => string.Empty,
+        };
+    }
+
+    public static string GetCssClassForReceiver(this ApplicationStatus status)
+    {
+        return status switch
+        {
+            ApplicationStatus.Pending => "govuk-tag govuk-tag--grey",
+            ApplicationStatus.Approved => "govuk-tag govuk-tag--blue",
+            ApplicationStatus.Rejected => "govuk-tag govuk-tag--red",
+            ApplicationStatus.Accepted => "govuk-tag govuk-tag--turquoise",
+            ApplicationStatus.FundsUsed => "govuk-tag govuk-tag--pink",
+            ApplicationStatus.Declined => "govuk-tag govuk-tag--yellow",
+            ApplicationStatus.Withdrawn => "govuk-tag govuk-tag--yellow",
+            ApplicationStatus.WithdrawnAfterAcceptance => "govuk-tag govuk-tag--yellow",
+            _ => string.Empty
+        };
     }
 }
